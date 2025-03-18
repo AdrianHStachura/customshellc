8a21592a9ea878f2fd7dfe4f9ba2f9a38b58c909
Adrian Stachura <48847246+AdrianHStachura@users.noreply.github.com>
2025-03-18 19:58:04 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,9 @@
+#include <stdio.h> // printf fgets()
+#include <stdlib.h> // malloc free exit()
+#include <string.h> // //strtok() strcmp()
+#include <unistd.h> //fork() execvp() chdir()
+#include <errno.h> // error handling perror()
+#include <sys/wait.h> // // process management waitpid()
+#include <sys/types.h> ////pid_t()
+
 
